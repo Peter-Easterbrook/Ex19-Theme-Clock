5d62efd3bcb9c15eaff332b0a408549bfbd5990b
Peter-Easterbrook <peter.easterbrook@gmail.com>
2022-03-20 16:23:32 +0100

    Attempt to fix hands issue

--- script.js
+++ script.js
@@ -54,21 +54,21 @@ function setTime() {
   hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
     hoursForClock,
     0,
-    11,
+    12,
     0,
     360
   )}deg)`;
   minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
     minutes,
     0,
-    59,
+    60,
     0,
     360
   )}deg)`;
   secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
     seconds,
     0,
-    59,
+    60,
     0,
     360
   )}deg)`;
